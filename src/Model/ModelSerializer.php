154d2f031de544939b92cf6c0a7a8e690418ac37
Keith Maika <keithm@aoeex.com>
2023-06-22 17:46:58 -0400

    Add a model serializer class and unit test.

---
+++ src/Model/ModelSerializer.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Kicken\Copyleaks\Model;
+
+class ModelSerializer {
+    public function serialize(object $model) : string{
+        return json_encode($this->serializeValue($model));
+    }
+
+    private function serializeValue($value){
+        if (is_scalar($value)){
+            return $value;
+        } else if (is_array($value)){
+            $value = array_map([$this, 'serializeValue'], $value);
+
+            return array_filter($value, function($item){
+                return $item !== null;
+            });
+        } else {
+            $result = new \stdClass();
+            $setProperties = false;
+            foreach (get_object_vars($value) as $name => $value){
+                if ($value === null){
+                    continue;
+                } else {
+                    $value = $this->serializeValue($value);
+                    if ($value !== null){
+                        $setProperties = true;
+                        $result->$name = $value;
+                    }
+                }
+            }
+
+            return $setProperties ? $result : null;
+        }
+    }
+}
